e3f4b4abba66d7c4df7b90197559c7c706edfa3a
murphy52589 <kylemurphy52589@gmail.com>
2024-06-10 20:03:57 -0500

    deleting pokemon project

--- react/pokemon/src/starter/PokemonStarter.tsx
+++
@@ -1,9 +0,0 @@
-export default function PokemonStarter()  {
-        return (
-            <form>
-                <label>Enter a pokemon name</label>
-                <input type="text" />
-                <button type="submit">Submit</button>
-            </form>
-        );
-}
